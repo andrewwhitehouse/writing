603156be61cea9b5435080b92b85ba85e1322807
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-05-15 12:48:07 +0100

    Test passing.

--- code/python/chapter1/chapter1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def circumference(radius):
-  return 2 * math.pi * radius
-
-
--- code/python/test_chapter1.py
+++ code/python/test_chapter1.py
@@ -4,7 +4,7 @@ from chapter1 import circumference
 class TestChapter1(unittest.TestCase):
 
     def test_circumference(self):
-        self.assertEqual(circumference(10), 62.8318)
+        self.assertAlmostEqual(circumference(10), 62.8318, places=3)
 
 if __name__ == '__main__':
     unittest.main()
