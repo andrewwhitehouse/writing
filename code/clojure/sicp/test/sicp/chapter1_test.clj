da620fbfaf80e5ced2a51aa089445db9cd66d384
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-05-20 15:55:23 +0100

    Adapt import approach

--- code/clojure/sicp/test/sicp/chapter1_test.clj
+++ code/clojure/sicp/test/sicp/chapter1_test.clj
@@ -1,12 +1,12 @@
 (ns sicp.chapter1-test
   (:require [clojure.test :refer :all]
-            [sicp.chapter1 :refer :all]))
+            [sicp.chapter1 :as ch1]))
 
 (defn is-close [a b tolerance]
   (< (Math/abs (- a b)) tolerance))
 
 (deftest test-square
   (testing "square integer."
-    (is (= 9 (square 3))))
+    (is (= 9 (ch1/square 3))))
   (testing "square decimal"
-    (is (is-close (square 2.2) 4.84 0.001))))
+    (is (is-close (ch1/square 2.2) 4.84 0.001))))
