b238444084768ece898500441f257b33b931cd09
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-05-24 08:13:10 +0100

    Two tests passing.

--- code/rust/ex1_3/src/lib.rs
+++ code/rust/ex1_3/src/lib.rs
@@ -1,5 +1,5 @@
-fn two_largest(_a: i32, _b: i32, _c: i32) -> [i32; 2] {
-    [3, 2]
+fn two_largest(a: i32, b: i32, _c: i32) -> [i32; 2] {
+    if a > b { [a, b] } else { [b, a] }
 }
 
 #[cfg(test)]
@@ -9,5 +9,10 @@ mod tests {
     #[test]
     fn should_return_first_two_if_in_descending_order() {
         assert_eq!([3, 2], two_largest(3, 2, 1));
+    }
+
+    #[test]
+    fn should_return_first_two_if_in_ascending_order() {
+        assert_eq!([3, 2], two_largest(2, 3, 1));
     }
 }
