22f0805cb410825500b1579b55d991afe35c05c3
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-05-24 07:47:41 +0100

    Add failing test

---
+++ code/rust/ex1_3/src/lib.rs
@@ -0,0 +1,13 @@
+fn two_largest(_a: i32, _b: i32, _c: i32) -> [i32; 2] {
+    [0, 0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_first_two_if_in_descending_order() {
+        assert_eq!([3, 2], two_largest(3, 2, 1));
+    }
+}
