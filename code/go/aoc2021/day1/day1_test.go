a6104c294d5ab17f81dce64db23924f7d7609d43
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-06-10 17:35:55 +0100

    Add initial day1 code.

---
+++ code/go/aoc2021/day1/day1_test.go
@@ -0,0 +1,12 @@
+package day1
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	depths := []int16{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	var expected uint16 = 7
+	actual := CountIncreases(depths)
+	if expected != actual {
+		t.Errorf("CountIncreases was incorrect, got: %d, want: %d.", actual, expected)
+	}
+}
--- code/go/aoc2021/main.go
+++ code/go/aoc2021/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"aoc2021/day1"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	depths := []int16{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	fmt.Println(day1.CountIncreases(depths))
 }
 
