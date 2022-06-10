b5ddbb0896e5b5a39b281075e3bb402ccdc879f8
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-06-10 18:02:23 +0100

    Rename test

--- code/go/aoc2021/day1/day1_test.go
+++ code/go/aoc2021/day1/day1_test.go
@@ -2,7 +2,7 @@ package day1
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestCountIncreases(t *testing.T) {
 	depths := []int16{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 	var expected uint16 = 7
 	actual := CountIncreases(depths)
