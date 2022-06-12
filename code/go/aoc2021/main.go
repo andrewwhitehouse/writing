a4d75ba0da749e8082f00d8919ad8365903710bd
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-06-12 11:06:45 +0100

    Day 1 Part 1 solved.

--- code/go/aoc2021/main.go
+++ code/go/aoc2021/main.go
@@ -3,8 +3,31 @@ package main
 import (
 	"aoc2021/day1"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
+func loadContent(fileName string) string {
+	fileBytes, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return string(fileBytes)
+}
+
+func solveDay1() {
+	content := loadContent("input/day1.txt")
+	part1Result, err := day1.Part1(content)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Day 1 Part 1 %d\n", part1Result)
+}
+
 func main() {
-	fmt.Println(day1.CountIncreases([]int16{1, 2, 3}))
+	solveDay1()
 }
