75e2438450fb9ed9d7c286023af56e2cc62d59d6
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-06-12 10:53:21 +0100

    Add integration test.

--- code/go/aoc2021/day1/day1.go
+++ code/go/aoc2021/day1/day1.go
@@ -1,6 +1,6 @@
 package day1
 
-func CountIncreases(depths []int16) uint16 {
+func CountIncreases(depths []uint16) uint16 {
 	var increases uint16 = 0
 	for i := 1; i < len(depths); i++ {
 		if depths[i] > depths[i-1] {
@@ -8,4 +8,12 @@ func CountIncreases(depths []int16) uint16 {
 		}
 	}
 	return increases
+}
+
+func Part1(content string) (uint16, error) {
+	values, err := Parse(content)
+	if err != nil {
+		return 0, err
+	}
+	return CountIncreases(values), nil
 }
--- code/go/aoc2021/day1/day1_test.go
+++ code/go/aoc2021/day1/day1_test.go
@@ -3,10 +3,18 @@ package day1
 import "testing"
 
 func TestCountIncreases(t *testing.T) {
-	depths := []int16{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+	depths := []uint16{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 	var expected uint16 = 7
 	actual := CountIncreases(depths)
 	if expected != actual {
 		t.Errorf("CountIncreases was incorrect, got: %d, want: %d.", actual, expected)
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	actual, _ := Part1("199\n200\n208\n210\n200\n207\n240\n269\n260\n263")
+	expected := uint16(7)
+	if expected != actual {
+		t.Errorf("Part1 was incorrect, got: %d, want: %d.", actual, expected)
 	}
 }
